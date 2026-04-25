10eca8e54614794b359728c802c4040dfd17d42f
priya-10103 <bmw50998@gmail.com>
2026-04-25 12:29:36 +0530

    UI improved

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ function addNotice() {
     let input = document.getElementById("noticeInput");
     let text = input.value;
 
-    if(text === "") return;
+    if (text === "") return;
 
     let li = document.createElement("li");
-    li.innerHTML = text + ' <button class="delete-btn" onclick="removeNotice(this)">X</button>';
+    li.innerText = text;
 
     document.getElementById("noticeList").appendChild(li);
     input.value = "";
-}
-
-function removeNotice(btn) {
-    btn.parentElement.remove();
 }
